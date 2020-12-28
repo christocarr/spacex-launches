4e31dd78b41c8f6f09c29c867c8bc4452b97ab76
christocarr <christocarr@outlook.com>
2020-12-28 16:09:28 +0000

    Feature: add functionality to filter for chosen launch item

--- src/components/LaunchPage.js
+++ src/components/LaunchPage.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
+import AppContext from '../context/appContext';
 function LaunchPage() {
+	const { state } = useContext(AppContext);
+
 	let { id } = useParams();
 
-	return <div>Launch page {id}</div>;
+	const [launchItem, setLaunchItem] = useState({
+		mission_name: '',
+	});
+
+	useEffect(() => {
+		id = Number(id);
+		const chosenItem = state.launchData.filter(
+			(item) => item.flight_number === id
+		);
+		setLaunchItem(chosenItem[0]);
+	}, []);
+
+	console.log(launchItem);
+
+	return <div>{launchItem.mission_name}</div>;
 }
 
 export default LaunchPage;
